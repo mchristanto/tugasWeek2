8e6146f69f5b507f5d53c4d6fe1c93c53533eec7
Michell Christanto <mchristanto01@student.ciputra.ac.id>
2022-03-14 07:24:53 -0700

    selesai

--- tugas1/Form1.cs
+++ tugas1/Form1.cs
@@ -14,17 +14,16 @@ namespace tugas1
 
         private void FormAturWarna_Load(object sender, EventArgs e)
         {
-           
-            
         }
         private void btnProses_Click(object sender, EventArgs e)
         {
-
+            try
+            { 
             if (tBoxInput.Text.Substring(0, 4).ToLower() == "isi:")
             {
                 lblOutput.Text = tBoxInput.Text.Substring(4);
             }
-            if (tBoxInput.Text.ToLower() == "hide")
+           else if (tBoxInput.Text.ToLower() == "hide")
             {
                 lblOutput.Hide();
             }
@@ -32,38 +31,50 @@ namespace tugas1
             {
                 lblOutput.Show();
             }
-
+            else if (tBoxInput.Text.ToLower() == "perbesar")
+            {
+                lblOutput.Font = new Font(lblOutput.Font.Name, lblOutput.Font.Size + 1);
+            }
+            else if (tBoxInput.Text.ToLower() == "perkecil")
+            {
+                lblOutput.Font = new Font(lblOutput.Font.Name, lblOutput.Font.Size - 1);
+            }
             else if (tBoxInput.Text.Substring(0, 6).ToLower() == "warna:")
             {
-
-
-
-                if (tBoxInput.Text.Substring(6) == "default")
-                {
-
-                    lblOutput.ForeColor = Color.Black;
-                }
-                else if (tBoxInput.Text.Substring(6) == "red")
-                {
-
-                    lblOutput.ForeColor = Color.Red;
-                }
-                else if (tBoxInput.Text.Substring(6) == "blue")
-                {
-                    lblOutput.ForeColor = Color.Blue;
-                }
-                else if (tBoxInput.Text.Substring(6) == "green")
-                {
-                    lblOutput.ForeColor = Color.Green;
-                }
-
             }
-            
+            else if (tBoxInput.Text.Substring(6) == "default")
+            {
+                lblOutput.ForeColor = Color.Black;
+            }
+            else if (tBoxInput.Text.Substring(6) == "red")
+            {
+                lblOutput.ForeColor = Color.Red;
+            }
+            else if (tBoxInput.Text.Substring(6) == "blue")
+            {
+                lblOutput.ForeColor = Color.Blue;
+            }
+            else if (tBoxInput.Text.Substring(6) == "green")
+            {
+                lblOutput.ForeColor = Color.Green;
+            }
+
+            else if (tBoxInput.Text.ToLower() == "restart")
+            {
+                Application.Restart();
+            }
+        }
+             catch (Exception keluar)
+            {
+                MessageBox.Show("Sytax Tidak Ditemukan!");
+            } //
         }
         private void tBoxInput_TextChanged(object sender, EventArgs e)
         {
         }
-
-        
+       
     }
 }
+
+
+
